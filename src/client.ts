00db08266193a29756c9cccbb3412a3dd41977b9
Justin Griffith <justin.dean.griffith@gmail.com>
2021-10-09 07:29:37 -0500

    Remove file extension from imports

--- src/client.ts
+++ src/client.ts
@@ -3,8 +3,8 @@ import Bottleneck from 'bottleneck'
 import qs from 'qs'
 import isofetch from 'isomorphic-unfetch'
 
-import urls from './urls.js'
-import parse from './parse.js'
+import urls from './urls'
+import parse from './parse'
 
 import {
   RawAccount,
@@ -32,7 +32,7 @@ import {
   LastQuote_v1,
   LastTrade_v1,
   Snapshot,
-} from './entities.js'
+} from './entities'
 
 import {
   GetOrder,
@@ -62,7 +62,7 @@ import {
   GetLastQuote_v1,
   GetSnapshot,
   GetSnapshots,
-} from './params.js'
+} from './params'
 
 const unifetch = typeof fetch !== 'undefined' ? fetch : isofetch
 export class AlpacaClient {
